17161b423fbc031b91a8a9823998dd8b1ecb3914
Rishita <154823062+RishitaCS@users.noreply.github.com>
2026-07-11 22:46:10 +0530

    Update 07_change_over_time _analysis.sql

--- scripts/07_change_over_time _analysis.sql
+++ scripts/07_change_over_time _analysis.sql
@@ -13,3 +13,37 @@ SQL Functions Used:
 ===============================================================================
 */
 
+-- Analyse sales performance over time
+-- Quick Date Functions
+Select
+YEAR(order_date) as order_year,
+MONTH(order_date) AS order_month,
+SUM(sales_amount) as total_sales,
+COUNT(DISTINCT customer_key) as total_customers,
+SUM(quantity) as total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date), MONTH(order_date);
+
+-- DATETRUNC()
+SELECT
+DATETRUNC(month, order_date) AS order_date,
+SUM(sales_amount) AS total_sales,
+COUNT(DISTINCT customer_key) AS total_customers,
+SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month, order_date)
+ORDER BY DATETRUNC(month, order_date);
+
+-- FORMAT()
+SELECT
+FORMAT(order_date, 'yyyy-MMM') AS order_date,
+SUM(sales_amount) AS total_sales,
+COUNT(DISTINCT customer_key) AS total_customers,
+SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'yyyy-MMM')
+ORDER BY FORMAT(order_date, 'yyyy-MMM');
